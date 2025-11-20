2417fb718a58449d73a29d07424af1d5fd87ca83
StockRAT <eena.marr@gmail.com>
2025-11-20 12:28:40 +0100

    Removed unused folder and added skeleton file for most planetary condition

---
+++ Project_Oasis/src/com/fs/starfarer/api/impl/campaign/econ/PoorLigth.java
@@ -0,0 +1,19 @@
+package com.fs.starfarer.api.impl.campaign.econ;
+
+
+import com.fs.starfarer.api.campaign.econ.Industry;
+import com.fs.starfarer.api.ui.TooltipMakerAPI;
+import com.fs.starfarer.api.impl.campaign.ids.Industries;
+import com.fs.starfarer.api.impl.campaign.ids.Commodities;
+import com.fs.starfarer.api.util.Misc;
+
+public class PoorLigth extends BaseHazardCondition {
+    
+    public void apply(String id) {
+
+    }
+
+    public void unapply (String id) { 
+        
+    }
+}
